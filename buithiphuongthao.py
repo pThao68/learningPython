a3514be7dcd9c1fee4b2404afaf2653564bfeb3c
pThao68 <buithiphuongthao06082002@gmail.com>
2024-04-03 17:31:56 +0700

    feat: commit 2

--- buithiphuongthao.py
+++ buithiphuongthao.py
@@ -1,3 +1,12 @@
 # Đề bài
 # - Đọc 1 file văn bản gồm nhiều dòng
 # - Ghi ra màn hình theo thứ tự ngược lại của các dòng
+# ví du: 
+# Nam quốc sơn hà 
+# Nam đế cư
+# ==>
+# Nam đế cư
+# Nam quốc sơn hà
+with open('abc.txt', 'r') as file:
+    for line in reversed(list(file.readlines())):
+        print(line.strip())
